48ef5f2fcf5b37eb0f7f94304e85449f8cd964ef
thomsen4 <43766624+thomsen4@users.noreply.github.com>
2018-10-08 18:42:49 -0500

    Ignore PRactice

--- PRACTICE/index.js
+++ PRACTICE/index.js
@@ -1,4 +1,20 @@
-let x = 5;
-let y = 7;
-let z = 9;
-console.log(x+y+z);
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+var ROOT_DIR = "html/";
+
+var port = process.env.port || 1337
+http.createServer(function (req, res) {
+    var urlObj = url.parse(req.url, true, false);
+    fs.readFile(ROOT_DIR + urlObj.pathname, function (err, data) {
+        if (err) {
+            res.writeHead(404);
+            res.end(JSON.stringify(err));
+            return;
+        }
+        res.writeHead(200);
+        res.end(data);
+    });
+}).listen(port);
+console.log('Server running at http://127.0.0.1:1337/');
